5bc71866e57fa2db3e8e971a61193155ab880b0e
Érika Rocha Gonçalves <erikatrue@gmail.com>
2022-08-22 16:31:57 -0300

    Adicionando requisitos 8 e 9

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,40 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import profileIcon from '../images/profileIcon.svg';
 import searchIcon from '../images/searchIcon.svg';
 
 function Header({ title }) {
+  const [showBar, setShowBar] = useState(false);
+
+  const toggleSearchBar = () => {
+    setShowBar(!showBar);
+  };
+
   return (
     <div>
-      <img
-        data-testid="profile-top-btn"
-        src={ profileIcon }
-        alt="Profile icon"
-      />
+      <Link to="/profile">
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="Profile icon"
+        />
+      </Link>
       {(title === 'Drinks' || title === 'Foods')
-      && (<img
-        data-testid="search-top-btn"
-        src={ searchIcon }
-        alt="Search icon"
-      />)}
+      && (
+        <button
+          onClick={ toggleSearchBar }
+          type="button"
+        >
+          <img
+            data-testid="search-top-btn"
+            src={ searchIcon }
+            alt="Search icon"
+          />
+        </button>
+      )}
+      { showBar
+      && <input data-testid="search-input" type="text" /> }
       <h2 data-testid="page-title">{title}</h2>
     </div>
   );
