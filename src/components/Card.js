32927f035229d9c93cc8ba5f953e3fd899384461
Joao Espanha <jpbesr@gmail.com>
2022-08-23 15:18:33 -0300

    implenting req 14

---
+++ src/components/Card.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import React, { useContext } from 'react';
+import SearchContext from '../context/SearchContext';
+
+export default function Card({ index, isMeal }) {
+  const { apiResponse } = useContext(SearchContext);
+
+  return (
+    <div data-testid={ `${index}-recipe-card` }>
+      <img
+        data-testid={ `${index}-card-img` }
+        src={ isMeal
+          ? (apiResponse[index].strMealThumb) : (apiResponse[index].strDrinkThumb) }
+        alt={ isMeal ? (apiResponse[index].strMeal) : (apiResponse[index].strDrink) }
+      />
+      <h3
+        data-testid={ `${index}-card-name` }
+      >
+        {isMeal ? (apiResponse[index].strMeal) : (apiResponse[index].strDrink) }
+
+      </h3>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  index: PropTypes.number,
+}.isRequired;
--- src/pages/HomeFoods.js
+++ src/pages/HomeFoods.js
@@ -1,18 +1,29 @@
 import React, { useContext, useEffect } from 'react';
+import Card from '../components/Card';
 import Header from '../components/Header';
 import SearchContext from '../context/SearchContext';
 
 function HomeFoods() {
-  const { setCurrentCategory } = useContext(SearchContext);
+  const { setCurrentCategory, apiResponse } = useContext(SearchContext);
 
   useEffect(() => {
     setCurrentCategory('foods');
     // eslint-disable-next-line
   }, []);
 
+  const maximumReceipes = 12;
+
   return (
     <div>
       <Header title="Foods" />
+      {
+        apiResponse.map((receipe, index) => (
+          (index < maximumReceipes && (<Card
+            key={ index }
+            isMeal
+            index={ index }
+          />))))
+      }
     </div>
   );
 }
