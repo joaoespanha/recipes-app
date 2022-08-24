52a984c5860e8816c12f50606290e79bd009d42e
Joao Espanha <jpbesr@gmail.com>
2022-08-24 16:28:56 -0300

    alcancando req 23 uhuuul

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Card({ index, isMeal, recipeData }) {
   return (
-    <div data-testid={ `${index}-recipe-card` }>
-      <img
-        data-testid={ `${index}-card-img` }
-        src={ isMeal
-          ? (recipeData.strMealThumb) : (recipeData.strDrinkThumb) }
-        alt={ isMeal ? (recipeData.strMeal) : (recipeData.strDrink) }
-      />
-      <h3
-        data-testid={ `${index}-card-name` }
-      >
-        {isMeal ? (recipeData.strMeal) : (recipeData.strDrink) }
+    <Link
+      to={ isMeal ? `/foods/${recipeData.idMeal}` : `/drinks/${recipeData.idDrink}` }
+    >
+      <div data-testid={ `${index}-recipe-card` }>
+        <img
+          data-testid={ `${index}-card-img` }
+          src={ isMeal
+            ? (recipeData.strMealThumb) : (recipeData.strDrinkThumb) }
+          alt={ isMeal ? (recipeData.strMeal) : (recipeData.strDrink) }
+        />
+        <h3
+          data-testid={ `${index}-card-name` }
+        >
+          {isMeal ? (recipeData.strMeal) : (recipeData.strDrink) }
 
-      </h3>
-    </div>
+        </h3>
+      </div>
+    </Link>
   );
 }
 
